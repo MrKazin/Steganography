2f825a56dc8a8cc73df6d14b4366b711fecdbdab
MrKaz <MrKaz@DESKTOP-FK7J07S>
2021-09-09 17:57:43 +0400

    Add project files.

---
+++ MySolution/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace MySolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ImagePanel = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.buttonCrypt = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonLoad2 = new System.Windows.Forms.Button();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.ImagePanel2 = new System.Windows.Forms.Panel();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.PasswordBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.groupBox8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ImagePanel);
+            this.groupBox1.Location = new System.Drawing.Point(472, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 358);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image";
+            // 
+            // ImagePanel
+            // 
+            this.ImagePanel.Location = new System.Drawing.Point(6, 22);
+            this.ImagePanel.Name = "ImagePanel";
+            this.ImagePanel.Size = new System.Drawing.Size(362, 330);
+            this.ImagePanel.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TextBox);
+            this.groupBox2.Location = new System.Drawing.Point(6, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 201);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Text";
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(7, 22);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(439, 162);
+            this.TextBox.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.PasswordBox);
+            this.groupBox3.Location = new System.Drawing.Point(6, 213);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(460, 71);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Password";
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(7, 22);
+            this.PasswordBox.Multiline = true;
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(439, 33);
+            this.PasswordBox.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.buttonCrypt);
+            this.groupBox4.Controls.Add(this.buttonSave);
+            this.groupBox4.Controls.Add(this.buttonLoad);
+            this.groupBox4.Location = new System.Drawing.Point(6, 290);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(460, 74);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
+            // buttonCrypt
+            // 
+            this.buttonCrypt.Location = new System.Drawing.Point(168, 21);
+            this.buttonCrypt.Name = "buttonCrypt";
+            this.buttonCrypt.Size = new System.Drawing.Size(105, 34);
+            this.buttonCrypt.TabIndex = 2;
+            this.buttonCrypt.Text = "Crypt";
+            this.buttonCrypt.UseVisualStyleBackColor = true;
+            this.buttonCrypt.Click += new System.EventHandler(this.buttonCrypt_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(311, 21);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(106, 34);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save Picture";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(29, 21);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(105, 34);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load Picture";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(875, 407);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox4);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(867, 378);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Crypt";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox5);
+            this.tabPage2.Controls.Add(this.groupBox6);
+            this.tabPage2.Controls.Add(this.groupBox7);
+            this.tabPage2.Controls.Add(this.groupBox8);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(867, 378);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Decrypt";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.TextBox2);
+            this.groupBox5.Location = new System.Drawing.Point(6, 6);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(460, 201);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Text";
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(7, 22);
+            this.TextBox2.Multiline = true;
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(439, 162);
+            this.TextBox2.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.button1);
+            this.groupBox6.Controls.Add(this.buttonLoad2);
+            this.groupBox6.Location = new System.Drawing.Point(6, 290);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(460, 74);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "groupBox6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonLoad2
+            // 
+            this.buttonLoad2.Location = new System.Drawing.Point(69, 21);
+            this.buttonLoad2.Name = "buttonLoad2";
+            this.buttonLoad2.Size = new System.Drawing.Size(105, 34);
+            this.buttonLoad2.TabIndex = 0;
+            this.buttonLoad2.Text = "Load Picture";
+            this.buttonLoad2.UseVisualStyleBackColor = true;
+            this.buttonLoad2.Click += new System.EventHandler(this.buttonLoad2_Click);
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.ImagePanel2);
+            this.groupBox7.Location = new System.Drawing.Point(472, 6);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(374, 358);
+            this.groupBox7.TabIndex = 4;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Image";
+            // 
+            // ImagePanel2
+            // 
+            this.ImagePanel2.Location = new System.Drawing.Point(6, 22);
+            this.ImagePanel2.Name = "ImagePanel2";
+            this.ImagePanel2.Size = new System.Drawing.Size(362, 330);
+            this.ImagePanel2.TabIndex = 0;
+            // 
+            // groupBox8
+            // 
+            this.groupBox8.Controls.Add(this.PasswordBox2);
+            this.groupBox8.Location = new System.Drawing.Point(6, 213);
+            this.groupBox8.Name = "groupBox8";
+            this.groupBox8.Size = new System.Drawing.Size(460, 71);
+            this.groupBox8.TabIndex = 6;
+            this.groupBox8.TabStop = false;
+            this.groupBox8.Text = "Password";
+            // 
+            // PasswordBox2
+            // 
+            this.PasswordBox2.Location = new System.Drawing.Point(7, 22);
+            this.PasswordBox2.Multiline = true;
+            this.PasswordBox2.Name = "PasswordBox2";
+            this.PasswordBox2.Size = new System.Drawing.Size(439, 33);
+            this.PasswordBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 443);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = " PicturesCipher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox8.ResumeLayout(false);
+            this.groupBox8.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel ImagePanel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button buttonCrypt;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonLoad2;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Panel ImagePanel2;
+        private System.Windows.Forms.GroupBox groupBox8;
+        private System.Windows.Forms.TextBox PasswordBox2;
+    }
+}
+
---
+++ MySolution/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySolution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap bitmapOriginal;
+        private Bitmap bitmapModified;
+
+        //load crypt
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр5 Steganography";
+            openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                try
+                {
+                    bitmapOriginal = (Bitmap)Bitmap.FromFile(fileName);
+                    this.CenterToScreen();
+
+
+
+                    //get Graphics object for painting original
+                    Graphics gPanelOriginal = Graphics.FromHwnd(ImagePanel.Handle);
+
+                    //draw original bitmap into panel
+                    gPanelOriginal.DrawImage(bitmapOriginal, new Point(0, 0));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image. " + ex.Message);
+                }
+            }
+        }
+        //save crypt
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //saveFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр5 Steganography";
+            saveFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                bitmapModified.Save(fileName);
+            }
+        }
+        //load decrypt
+        private void buttonLoad2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр5 Steganography";
+            openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                try
+                {
+                    bitmapModified = (Bitmap)Bitmap.FromFile(fileName);
+                    this.CenterToScreen();
+
+
+
+                    //get Graphics object for painting original
+                    Graphics gPanelOriginal = Graphics.FromHwnd(ImagePanel2.Handle);
+
+                    //draw original bitmap into panel
+                    gPanelOriginal.DrawImage(bitmapModified, new Point(0, 0));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image. " + ex.Message);
+                }
+            }
+        }
+
+        //crypt
+        private void buttonCrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //show wait cursor
+                this.Cursor = Cursors.WaitCursor;
+
+                //start off with copy of original image
+                bitmapModified = new Bitmap(bitmapOriginal, bitmapOriginal.Width, bitmapOriginal.Height);
+                string encryptedBytes = CipherClass.Encrypt(TextBox.Text, PasswordBox.Text);
+                MessageBox.Show("Encrypted Bytes: "+encryptedBytes);
+
+                //get original message to be hidden
+                int numberbytes = (byte)encryptedBytes.Length * 2;
+                byte[] bytesOriginal = new byte[numberbytes + 1];
+                bytesOriginal[0] = (byte)numberbytes;
+
+                //// RJD algoritm
+                Encoding.UTF8.GetBytes(encryptedBytes, 0, encryptedBytes.Length, bytesOriginal, 1);
+
+                //set bits 1, 2, 3 of byte into LSB red
+                //set bits 4, 5, 6 of byte into LSB green
+                //set bits 7 and 8 of byte into LSB blue
+                int byteCount = 0;
+                for (int i = 0; i < bitmapOriginal.Width; i++)
+                {
+                    for (int j = 0; j < bitmapOriginal.Height; j++)
+                    {
+                        if (bytesOriginal.Length == byteCount)
+                            return;
+                        Color clrPixelOriginal = bitmapOriginal.GetPixel(i, j);
+                        byte r = (byte)((clrPixelOriginal.R & ~0x7) | (bytesOriginal[byteCount] >> 0) & 0x7);
+                        byte g = (byte)((clrPixelOriginal.G & ~0x7) | (bytesOriginal[byteCount] >> 3) & 0x7);
+                        byte b = (byte)((clrPixelOriginal.B & ~0x3) | (bytesOriginal[byteCount] >> 6) & 0x3);
+                        byteCount++;
+                        //set pixel to modified color
+                        bitmapModified.SetPixel(i, j, Color.FromArgb(r, g, b));
+                    }
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error hiding message." + ex.Message);
+            }
+            finally
+            {
+                //show normal cursor
+                this.Cursor = Cursors.Arrow;
+                //repaint
+                Invalidate();
+            }
+        }
+        //decrypt
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] bytesExtracted = new byte[256 + 1];
+            try
+            {
+                //show wait cursor, can be time-consuming
+                this.Cursor = Cursors.WaitCursor;
+
+                //get bits 1, 2, 3 of byte from LSB red
+                //get bits 4, 5, 6 of byte from LSB green
+                //get bits 7 and 8 of byte from LSB blue
+                int byteCount = 0;
+                for (int i = 0; i < this.bitmapModified.Width; i++)
+                {
+                    for (int j = 0; j < this.bitmapModified.Height; j++)
+                    {
+                        if (bytesExtracted.Length == byteCount)
+                            return;
+
+                        Color clrPixelModified = this.bitmapModified.GetPixel(i, j);
+                        byte bits123 = (byte)((clrPixelModified.R & 0x7) << 0);
+                        byte bits456 = (byte)((clrPixelModified.G & 0x7) << 3);
+                        byte bits78 = (byte)((clrPixelModified.B & 0x3) << 6);
+                        bytesExtracted[byteCount] = (byte)(bits78 | bits456 | bits123);
+                        byteCount++;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error extracting message." + ex.Message);
+            }
+            finally
+            {
+                //show normal cursor
+                this.Cursor = Cursors.Arrow;
+
+                //get number of bytes from start of array
+                int numberbytes = bytesExtracted[0];
+
+                //get remaining bytes in array into string
+                char[] cipheredBytes = Encoding.UTF8.GetString(bytesExtracted, 1, numberbytes).ToCharArray();
+                string g = "";
+                for (int i = 0; i < cipheredBytes.Length - cipheredBytes.Length / 2; g += cipheredBytes[i], i++) ;
+                string variable = g;
+
+                MessageBox.Show(variable, g + " " + cipheredBytes.Length);
+
+                TextBox2.Text = CipherClass.Decrypt(g, PasswordBox2.Text);
+
+
+            }
+        }
+    }
+}
